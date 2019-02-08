2f9bc48a26dbac6bcc58fcc33c5aeed6c9de539d
Tarang <tsgoten@gmail.com>
2019-02-08 12:29:51 -0500

    Initial Commit

---
+++ TrafficSegues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by Tarang Srivastava on 2/8/19.
+//  Copyright © 2019 Tarang Srivastava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
