2f1edd81eff5f6d11e0b6a423f379d625d7c151c
Tarang <tsgoten@gmail.com>
2019-02-15 11:55:36 -0500

    finished programming segue

--- TrafficSegues/ViewController.swift
+++ TrafficSegues/ViewController.swift
@@ -9,23 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     
-    @IBOutlet weak var textField: UITextField!
-    
+    @IBOutlet weak var segueSwitch: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        segue.destination.navigationItem.title = textField.text
+    @IBAction func yellowButtonTapped(_ sender: UIButton) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Yellow", sender: nil)
+        }
     }
     
-    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {
-        
+    @IBAction func greenButtonTapped(_ sender: UIButton) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Green", sender: nil)
+        }
     }
+    
 
 
 }
